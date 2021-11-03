399b8dcdd1d997ec8f800a4c7798c902c61e0b31
akio0911 <akio0911@gmail.com>
2021-11-04 08:12:01 +0900

    feedback

--- yutaro81Kadai8/RedViewController.swift
+++ yutaro81Kadai8/RedViewController.swift
@@ -8,17 +8,21 @@
 import UIKit
 
 class RedViewController: UIViewController {
-    @IBOutlet private weak var redLabel: UILabel!
+    @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var slider: UISlider!
+
     @IBAction private func changeValueSlider(_ sender: Any) {
         if let delegate = UIApplication.shared.delegate as? AppDelegate {
             delegate.num1 = slider.value
-            self.redLabel.text = "\(delegate.num1)" }
+            label.text = "\(delegate.num1)"
+        }
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if let delegate = UIApplication.shared.delegate as? AppDelegate {
             slider.value = delegate.num1
-            self.redLabel.text = "\(delegate.num1)" }
+            label.text = "\(delegate.num1)"
+        }
     }
 }
